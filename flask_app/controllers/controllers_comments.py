e8b8da48c3b042d49e856d999815a673de77ae6f
Brandon Colangelo <brandoncancode@gmail.com>
2023-07-03 16:04:36 -0400

    Removed adding comments to posts

--- flask_app/controllers/controllers_comments.py
+++
@@ -1,3 +0,0 @@
-from flask_app import app
-from flask import render_template, redirect, request, session, flash
-from flask_app.models import models_user, models_post, models_comment
--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 from flask_app import app
-from flask_app.controllers import controllers_users, controllers_posts, controllers_comments
-
+from flask_app.controllers import controllers_users, controllers_posts
 
 
 
